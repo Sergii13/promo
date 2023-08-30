23002b69949a6fb6d41886e44cce157583f840e3
sergii13 <sergii.khilchuk13@gmail.com>
2023-08-30 18:29:54 +0300

    add new fix

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -10,3 +10,10 @@ function scrollToBottom() {
 
 // Викликаємо функцію одразу після завантаження сторінки та при оновленні контенту
 scrollToBottom();
+const btnAll = document.querySelector(".benefit__button-all");
+if (btnAll) {
+  btnAll.addEventListener("click", (e) => {
+    e.preventDefault();
+    btnAll.closest(".benefit").classList.add("benefit--active");
+  });
+}
